da32aaae4d90f14188c7aa50e6ee911e0930c599
Shaked Eyal <shaked.eyal@gmail.com>
2018-03-22 11:45:03 +0200

    Added builder. Check if it's a pptx fie, else throw exception

--- Presentation.py
+++ Presentation.py
@@ -1,7 +1,12 @@
 from pptx import Presentation
+from pathlib import Path
 
 
-class Presentation:
+class Pres(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, path):
+        my_file = Path(path)
+        if my_file.is_file() and (path.endswith('.pptx')):
+            self.prs = Presentation(path)
+        else:
+            raise Exception("No Presentation is found")
