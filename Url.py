ac618ea0e4a927ae30a904ffdf2380959ad28735
Alexander Kremyanskyy <ALEXKREM@ibm.com>
2018-03-22 14:19:22 +0200

    Url

---
+++ Url.py
@@ -0,0 +1,20 @@
+import cStringIO
+import urllib
+
+
+class Url:
+    def __init__(self,path,flag):
+        self.path=path
+        self.flag=flag
+
+    def getContent(self,slide_number):
+        if self.flag=="img":
+            urllib.urlretrieve(self.path, "/"+slide_number+"/"+self.path.rsplit('/', 1)[-1])
+        if self.flag=="article":
+
+
+
+
+
+
+
