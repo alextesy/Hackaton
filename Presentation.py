96c7ef924ae3541e20b152a754f7a9fdb6285431
Shaked Eyal <shaked.eyal@gmail.com>
2018-03-22 12:21:06 +0200

    Commit before merge

--- Presentation.py
+++ Presentation.py
@@ -1,12 +1,28 @@
-from pptx import Presentation
 from pathlib import Path
+from pptx import Presentation
+
+from Slide import Slide
 
 
 class Pres(object):
 
-    def __init__(self, path):
+    def __init__(self, path, course):
         my_file = Path(path)
         if my_file.is_file() and (path.endswith('.pptx')):
             self.prs = Presentation(path)
+            self.course = course
+            self.slides = []
+            self.stop_words = set(line.strip() for line in open('stop_words.txt'))
         else:
             raise Exception("No Presentation is found")
+
+    def initialize(self):
+        for slide in self.prs.slides:
+            if not slide.shapes.title.has_text_frame:
+                continue
+            title = self.parse_title(slide.shapes.title.text)
+            s = Slide(title, "", self.prs.slides.index(slide), False, False, False, False)
+
+    def parse_title(self, title):
+        pass
+        # complete
--- Slide.py
+++ Slide.py
@@ -1,15 +1,12 @@
-from pptx import Presentation
-
-
 class Slide:
-    def __init__(self,title,content,slideNum,picture,text,meme,video):
+    def __init__(self, title, content, slideNum, picture, text, meme, video):
         self.title = title
-        self.content=content
+        self.content = content
         self.slideNum = slideNum
-        self.picture =picture
-        self.text=text
-        self.meme=meme
-        self.video=video
-    def find_links(self):
-        return findLinks(self)
+        self.picture = picture
+        self.text = text
+        self.meme = meme
+        self.video = video
 
+    def find_links(self):
+        pass
