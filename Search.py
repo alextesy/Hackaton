92eb7ad603af6320ff4f632c4ac1fb258036828e
Elior <Nehemya@post.bgu.ac.il>
2018-03-22 21:43:07 +0200

    Slide.py

---
+++ Search.py
@@ -0,0 +1,33 @@
+import os
+from Slide import Slide
+from google import google
+
+
+
+def searcher(slide):
+    query=slide.title
+    mypath = 's'+ str(slide.slideNum)
+    if not os.path.isdir(mypath):
+        os.makedirs(mypath)
+    s1 = "googleimagesdownload --keywords \""+query+"\" --limit 1 -o "+mypath
+    s2="googleimagesdownload --keywords \""+query+" meme\" --limit 1 -o "+mypath
+    os.system(s1)  #save pic
+    os.system(s2)
+    advancedQuery = query + ' site:reuters.com'
+    google_result = google.search(advancedQuery, 2)
+    for res in google_result:
+        i=0
+        if i>10:
+            continue
+        slide.url.append(res.link)
+
+
+
+def runSearch(slides):
+    for s in slides:
+        searcher(s)
+
+
+
+
+
