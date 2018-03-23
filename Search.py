b4bc1f402b7b2a127d2ac20db6a9ff46fea8d594
talbs254 <33547680+talbs254@users.noreply.github.com>
2018-03-23 03:14:42 +0300

    articles searcher

--- Search.py
+++ Search.py
@@ -1,6 +1,8 @@
 import os
-from types import NoneType
-
+from PIL import Image
+from google import google
+from langdetect import detect
+import imgkit
 from google import google
 
 
@@ -26,6 +28,57 @@ def searcher(slide):
         i += 1
 
 
+def articles(slide):
+    query = slide.title
+    advancedQuery = query + ' site:reuters.com'
+    google_result = google.search(advancedQuery, 1)
+    idx = 0
+    for res in google_result:
+        try:
+            idx += 1
+            lang = detect(res.description)
+            if lang == 'en':
+                path_wkhtmltoimage = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage.exe'
+                config = imgkit.config(wkhtmltoimage=path_wkhtmltoimage)
+                article_path = "Articles/" + query + str(idx) + '.jpg'
+                imgkit.from_url(res.google_link, article_path, config=config)
+                img = Image.open(article_path)
+                imgs2 = img.crop((0, 200, 1000, 1000))
+                imgs2.save(query + str(idx) + '.jpg')
+        except Exception:
+            os.remove(query + str(idx) + '.jpg')
+            idx -= 1
+
+
 def runSearch(slides):
     for s in slides:
         searcher(s)
+
+
+'''
+import os
+from PIL import Image
+from google import google
+from langdetect import detect
+import imgkit
+
+query = 'eye egg'
+advancedQuery = query + ' site:reuters.com'
+google_result = google.search(advancedQuery, 2)
+idx = 0
+for res in google_result:
+    try:
+        idx += 1
+        lang = detect(res.description)
+        if lang == 'en':
+            path_wkhtmltoimage = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage.exe'
+            config = imgkit.config(wkhtmltoimage=path_wkhtmltoimage)
+            imgkit.from_url(res.google_link, query + str(idx) + '.jpg', config=config)
+            img = Image.open(query + str(idx) + '.jpg')
+            imgs2 = img.crop((0, 200, 1000, 1000))
+            imgs2.save(query + str(idx) + '.jpg')
+    except Exception:
+        os.remove(query + str(idx) + '.jpg')
+        idx -= 1
+
+'''
